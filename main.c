ef257b611105274eaa1656f65fd011d97f1c9954
cocomong11 <carpediem1011@naver.com>
2025-10-02 15:27:35 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,17 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	printf("정수 하나 를 입력하세요:");
-	scanf("%d",&i);
-	if (i>=0){
-			printf("절댓값은 %d입니다.",i);
-	}
-
-	else{
-		printf("절댓값은 %d입니다.",-i);
-
-	}
-	
+	int i,a,sum=0;
+printf("input a number:");
+scanf("%d",&a);
+for(i=0;i<=a;i++){
+	sum+=i;
+}
+	printf("The result is %d",sum);
 }
 
